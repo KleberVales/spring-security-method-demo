f96a235f2639084dd3fbd92fbe56e3584ab0f8e2
Kleber Vales <klebervales.dev@gmail.com>
2025-08-15 12:46:55 -0300

    build: setup initial

---
+++ src/main/java/com/example/springsecuritymethoddemo/SpringSecurityMethodDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritymethoddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMethodDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMethodDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritymethoddemo/SpringSecurityMethodDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritymethoddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMethodDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
